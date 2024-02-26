0774c345cf130bbdf20c254a8975c20c98d90a34
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2024-02-25 17:46:46 -0800

    added prelim parse logic

--- brltty.sh
+++ brltty.sh
@@ -1,1 +1,34 @@
 #! /bin/bash
+
+# this script is called by ./brltty.sh brltty.conf
+# it will parse the brltty configuration file and print the following information:
+# - the number of braille-drivers
+# - the list of braille-devices
+# - the number of text tables
+# - the number of contraction tables
+
+# assign config file to variable
+configfile=$1
+
+# check if config file exists
+if [ ! -f $configfile ]; then
+  echo "Error: $configfile does not exist"
+  exit 1
+fi
+
+# get the number of braille-drivers "braille-driver"
+# grep -c will count the number of lines that contain "braille-driver"
+braille_drivers=$(grep -c "braille-driver" $configfile)
+
+# get the list of braille-devices "braille-device"
+# grep "braille-device" will print the line that contains "braille-device"
+# awk '{print $2}' will print the second column of the output
+braille_devices=$(grep "braille-device" $configfile | awk '{print $2}')
+
+# get the number of text tables "text-table"
+# grep -c will count the number of lines that contain "text-table"
+text_tables=$(grep -c "text-table" $configfile)
+
+# get the number of contraction tables "contraction-table"
+# grep -c will count the number of lines that contain "contraction-table"
+contraction_tables=$(grep -c "contraction-table" $configfile)
