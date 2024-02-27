4518b1ba144199efc84ba15c74a24e835d3d86be
efong20 <efong20@hawaii.edu>
2024-02-26 16:34:34 -0800

    prints all numbers, just need #2

--- brltty.sh
+++ brltty.sh
@@ -16,19 +16,23 @@ if [ ! -f $configfile ]; then
   exit 1
 fi
 
-# get the number of braille-drivers "braille-driver"
+# get the number of braille-drivers "#braille-driver"
 # grep -c will count the number of lines that contain "braille-driver"
-braille_drivers=$(grep -c "braille-driver" $configfile)
+braille_drivers=$(grep -c "#braille-driver" $configfile)
+echo $braille_drivers
 
 # get the list of braille-devices "braille-device"
 # grep "braille-device" will print the line that contains "braille-device"
 # awk '{print $2}' will print the second column of the output
 braille_devices=$(grep "braille-device" $configfile | awk '{print $2}')
+echo $braille_devices
 
-# get the number of text tables "text-table"
+# get the number of text tables "#text-table"
 # grep -c will count the number of lines that contain "text-table"
-text_tables=$(grep -c "text-table" $configfile)
+text_tables=$(grep -c "#text-table" $configfile)
+echo $text_tables
 
-# get the number of contraction tables "contraction-table"
+# get the number of contraction tables "#contraction-table"
 # grep -c will count the number of lines that contain "contraction-table"
-contraction_tables=$(grep -c "contraction-table" $configfile)
+contraction_tables=$(grep -c "#contraction-table" $configfile)
+echo $contraction_tables
